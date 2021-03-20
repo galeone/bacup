3dd08dc0f25d27ac343526fff0235dd3852248d5
Paolo Galeone <nessuno@nerdz.eu>
2021-03-20 15:51:18 +0100

    folders: implement errors and add tests

--- src/services/folders.rs
+++ src/services/folders.rs
@@ -1,24 +1,39 @@
-use std::path::{Path, PathBuf};
-
 use glob::glob;
+use std::fmt;
+use std::path::{Path, PathBuf};
 
 pub struct Folder {
     pub paths: Vec<PathBuf>,
 }
 
-#[derive(Debug)]
-pub enum FolderError {
-    IsNotAbsolute,
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    IsNotAbsolute(PathBuf),
     DoesNotExist(PathBuf),
 }
 
+impl std::error::Error for Error {}
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::IsNotAbsolute(path) => write!(f, "Path {} is not absolute", path.display()),
+            Error::DoesNotExist(path) => write!(f, "Path {} does not exist", path.display()),
+        }
+    }
+}
+
 impl Folder {
-    pub fn new(pattern: &str) -> Result<Folder, FolderError> {
+    pub fn new(pattern: &str) -> Result<Folder, Error> {
         if pattern.contains("*") {
             let base_path = pattern.split("*").next().unwrap();
             let base_path = Path::new(base_path);
+
             if !base_path.is_absolute() {
-                return Err(FolderError::IsNotAbsolute);
+                return Err(Error::IsNotAbsolute(PathBuf::from(base_path)));
+            }
+
+            if !base_path.exists() {
+                return Err(Error::DoesNotExist(PathBuf::from(base_path)));
             }
 
             return Ok(Folder {
@@ -30,10 +45,57 @@ impl Folder {
         }
         let path = Path::new(pattern);
         if !path.is_absolute() {
-            return Err(FolderError::IsNotAbsolute);
+            return Err(Error::IsNotAbsolute(PathBuf::from(path)));
+        }
+        if !path.exists() {
+            return Err(Error::DoesNotExist(PathBuf::from(path)));
         }
         return Ok(Folder {
             paths: vec![PathBuf::from(path)],
         });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_new_relative() {
+        let relative = "relative";
+        assert!(Folder::new(relative).is_err());
+        assert_eq!(
+            Folder::new(relative).err(),
+            Some(Error::IsNotAbsolute(PathBuf::from(relative)))
+        );
+    }
+
+    #[test]
+    fn test_new_absolute() {
+        let cwd = std::env::current_dir().unwrap();
+        assert!(Folder::new(cwd.to_str().unwrap()).is_ok());
+    }
+
+    #[test]
+    fn test_wildcard() {
+        let cwd = std::env::current_dir().unwrap();
+        let pattern = cwd.join("*");
+        let folder = Folder::new(pattern.to_str().unwrap());
+        assert!(folder.is_ok());
+        let folder = folder.unwrap();
+
+        let cargo = cwd.join("Cargo.toml");
+        assert!(folder.paths.contains(&cargo));
+    }
+
+    #[test]
+    fn test_non_existing_abolute() {
+        let cwd = std::env::current_dir()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .join("fakfakefakefake");
+        assert!(Folder::new(cwd.to_str().unwrap()).is_err());
     }
 }
