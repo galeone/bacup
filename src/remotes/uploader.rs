a5792f892d46fc9b3b1a5cb71207d4e7326ea1ad
Paolo Galeone <nessuno@nerdz.eu>
2021-04-02 15:02:46 +0200

    Added shared methods to Uploader trait

--- src/remotes/uploader.rs
+++ src/remotes/uploader.rs
@@ -1,10 +1,19 @@
 use async_trait::async_trait;
 
 use std::fmt;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::string::String;
 
+use chrono::DateTime;
+use chrono::Utc;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::io::prelude::*;
+
 use dyn_clone::DynClone;
+
+use tempfile::NamedTempFile;
 
 #[derive(Debug)]
 pub enum Error {
@@ -40,17 +49,63 @@ impl fmt::Display for Error {
 
 #[async_trait]
 pub trait Uploader: DynClone {
-    async fn upload_file(&self, path: PathBuf, remote_path: PathBuf) -> Result<(), Error>;
-    async fn upload_folder(&self, path: PathBuf, remote_path: PathBuf) -> Result<(), Error>;
-    async fn upload_file_compressed(
-        &self,
-        path: PathBuf,
-        remote_path: PathBuf,
-    ) -> Result<(), Error>;
-    async fn upload_folder_compressed(
-        &self,
-        path: PathBuf,
-        remote_path: PathBuf,
-    ) -> Result<(), Error>;
+    async fn upload_file(&self, path: &Path, remote_path: &Path) -> Result<(), Error>;
+    async fn upload_folder(&self, paths: &Vec<PathBuf>, remote_path: &Path) -> Result<(), Error>;
+    async fn upload_file_compressed(&self, path: &Path, remote_path: &Path) -> Result<(), Error>;
+    async fn upload_folder_compressed(&self, path: &Path, remote_path: &Path) -> Result<(), Error>;
     fn name(&self) -> String;
+
+    fn compress_folder(&self, path: &Path) -> Result<NamedTempFile, Error> {
+        let archive_path = NamedTempFile::new()?;
+
+        let archive = std::fs::File::create(&archive_path)?;
+        let mut encoder = GzEncoder::new(archive, Compression::default());
+        {
+            let mut tar = tar::Builder::new(&mut encoder);
+            tar.append_dir_all(".", path.clone())?;
+        }
+        let enc_res = encoder.finish();
+        if enc_res.is_err() {
+            return Err(Error::CompressionError);
+        }
+        Ok(archive_path)
+    }
+
+    fn compress_file(&self, path: &Path) -> Result<Vec<u8>, Error> {
+        let mut content: Vec<u8> = vec![];
+        let mut file = match std::fs::File::open(path.clone()) {
+            Ok(file) => file,
+            Err(error) => return Err(Error::LocalError(error)),
+        };
+
+        file.read_to_end(&mut content)?;
+
+        let mut e = GzEncoder::new(Vec::new(), Compression::default());
+        e.write_all(&content)?;
+
+        match e.finish() {
+            Ok(bytes) => return Ok(bytes),
+            Err(_) => return Err(Error::CompressionError),
+        }
+    }
+
+    fn remote_archive_path(&self, remote_path: &Path) -> PathBuf {
+        let now: DateTime<Utc> = Utc::now();
+        let parent = remote_path.parent().unwrap();
+        parent.join(format!(
+            "{}-{}.tar.gz",
+            now.format("%Y-%m-%d-%H.%M"),
+            remote_path.file_name().unwrap().to_str().unwrap()
+        ))
+    }
+
+    fn remote_compressed_file_path(&self, remote_path: &Path) -> PathBuf {
+        let now: DateTime<Utc> = Utc::now();
+        let parent = remote_path.parent().unwrap();
+        parent.join(format!(
+            "{}-{}.gz",
+            now.format("%Y-%m-%d-%H.%M"),
+            remote_path.file_name().unwrap().to_str().unwrap()
+        ))
+    }
 }
